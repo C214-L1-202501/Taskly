2fa37d46673300b5a2e5ef4f0013c1beee288ef8
lucas belino <lucas.belino@incoway.com.br>
2025-02-28 22:48:50 -0300

    Atualização do route

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,15 @@ bp = Blueprint("main", __name__)
 @bp.route("/")
 def home():
     return render_template("home.html")
+
+@bp.route("/tarefas")
+def task():
+    return render_template("task.html")
+
+@bp.route("/goal")
+def goal():
+    return render_template("goal.html")
+
+@bp.route("/history")
+def history():
+    return render_template("history.html")
