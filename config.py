8d9024852891f8ba03544dc8a339bbc6746e5491
Gustavo Ribeiro de Oliveira <gustavoribeirogr253@gmail.com>
2025-03-13 18:13:58 -0300

    :sparkles: feat: Configuration of sql alchemy

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ BASEDIR = os.path.dirname(os.path.abspath("main.py"))
 STATIC_FOLDER = "static"
 TEMPLATE_FOLDER = "templates"
 
+
 class Config:
     FLASK_DEBUG = 1
-    SESSION_TYPE = 'filesystem'
+    SESSION_TYPE = "filesystem"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'banco.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
