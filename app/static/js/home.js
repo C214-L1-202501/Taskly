d668c41e331498caa28d0a4531fcde9c9723a089
Gustavo Ribeiro de Oliveira <gustavoribeirogr253@gmail.com>
2025-06-12 13:19:26 -0300

    :sparkles: feat: melhorado home

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,19 +1,9 @@
 let current = 1;
 const total = 5;
-let direction = 1;
-
+console.log("asdasidnaosndoansdkjansdk")
 function autoSlideCarousel() {
     document.getElementById(`carousel${current}`).checked = true;
-
-    current += direction;
-
-    if (current > total) {
-        current = total - 1;
-        direction = -1;
-    } else if (current < 1) {
-        current = 2;
-        direction = 1;
-    }
+    current = current % total + 1;
 }
 
 setInterval(autoSlideCarousel, 3000);
